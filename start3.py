c977b0b942c6e8d54576eefcb2a7b43a6bb7d17f
Sussi Zällh <sussi.zallh@gmail.com>
2015-08-05 11:35:35 +0200

    Skrev om  programmet med klass och metod

--- start3.py
+++ start3.py
@@ -1,4 +1,6 @@
 class Product(object):
+	def price_with_vat(self):
+		return self.price*self.count*self.vat
 	price=0
 	count=0
 	vat=1
@@ -10,7 +12,8 @@ robot.vat=1.25
 
 book=Product()
 book.price=100
+book.count=1
 book.vat=1.06
 
 
-print robot.price*robot.count*robot.vat+book.price*book.vat
+print robot.price_with_vat() + book.price_with_vat()
