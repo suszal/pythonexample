6ff5cefcd9f2bdd5c8a456b4ba12197ecd025045
Sussi Zällh <sussi.zallh@gmail.com>
2015-08-05 11:44:59 +0200

    skrev om programmet med klass och konstruktör

--- start3.py
+++ start3.py
@@ -1,19 +1,17 @@
 class Product(object):
-	def price_with_vat(self):
-		return self.price*self.count*self.vat
 	price=0
 	count=0
 	vat=1
+	def __init__(self, price, count,vat):
+		self.price=price
+		self.count=count
+		self.vat=vat
+	def price_with_vat(self):
+		return self.price*self.count*self.vat
+	
 
-robot=Product()
-robot.price=900
-robot.count=2
-robot.vat=1.25
-
-book=Product()
-book.price=100
-book.count=1
-book.vat=1.06
+robot=Product(price=900, count=2, vat=1.25)
+book=Product(price=100, count=1, vat=1.06)
 
 
 print robot.price_with_vat() + book.price_with_vat()
